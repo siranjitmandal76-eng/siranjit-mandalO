aeea61a9d739c7db6bb36aaeb0be59488cbf3506
siranjitmandal76-eng <siranjitmandal76@gmail.com>
2025-12-28 00:07:03 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-<a href="pdf/trading-plan.pdf" target="_blank" download>
+<section>
+<h2>📄 Trading Plan PDF</h2>
+<div style="text-align:center">
+  <a href="./trading-plan.pdf" 
+     class="btn" 
+     target="_blank">
+     📥 Open Trading Plan
+  </a>
+</div>
+</section>
